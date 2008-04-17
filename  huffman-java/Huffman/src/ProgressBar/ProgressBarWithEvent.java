c0f597a57320b0a78a40fc8c0d82730bdc195b1b
quentin.douillet <quentin.douillet@1136dfab-0d4b-0410-9e11-5134f6b347ec>
2008-04-17 11:56:33 +0000

    Le code de la barre de progression (fonctionne avec des évènements)

---
+++  huffman-java/Huffman/src/ProgressBar/ProgressBarWithEvent.java
@@ -0,0 +1,25 @@
+package ProgressBar;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.JProgressBar;
+
+@SuppressWarnings("serial")
+public class ProgressBarWithEvent extends JProgressBar implements PropertyChangeListener {
+	
+	public ProgressBarWithEvent(){
+		super();
+		this.setMinimum(0);
+		this.setMaximum(100);
+		this.setValue(0);
+		this.setStringPainted(true);
+	}
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("progress")){
+			this.setValue((Integer)evt.getNewValue());
+		}
+	}
+	
+}
